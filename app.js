98d54d826a591954fd670c77b4afde35fe9fe041
Your Name <Tahani@Tahani-Almanies-MacBook-Pro.local>
2015-02-14 08:10:52 -0700

    changed

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ require('./mongo/issueList')(app)
 require('./mongo/issueView')(app)
 
 
-var server = app.listen(3000, function() {
+var server = app.listen(process.env.PORT || 5000, function() {
 
     var host = server.address().address
     var port = server.address().port
